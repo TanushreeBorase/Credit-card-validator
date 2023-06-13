4008542cb0f4e9fabba26504e802f074ed1dbc67
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-13 10:29:46 +0530

    Update CREDIT CARD VALIDATOR.cpp

--- CREDIT CARD VALIDATOR.cpp
+++ CREDIT CARD VALIDATOR.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 
-// Return this number if it is a single digit, otherwise,
-// return the sum of the two digits
+// Return this number if it is a single digit, otherwise,return the sum of the two digits
+
 int getDigit(int number)
 {
 if (number < 9)
@@ -18,26 +18,32 @@ string num = to_string(d);
 return num.length();
 }
 
-// Return the first k number of digits from
-// number. If the number of digits in number
+// Return the first k number of digits from number.
+// If the number of digits in number
 // is less than k, return number.
 long getPrefix(long number, int k)
+
 {
 if (getSize(number) > k)
 {
 	string num = to_string(number);
 	return stol(num.substr(0, k));
 }
+	
 return number;
+	
 }
 
+
 // Return true if the digit d is a prefix for number
+
 bool prefixMatched(long number, int d)
 {
 return getPrefix(number, getSize(d)) == d;
 }
 
 // Get the result from Step 2
+
 int sumOfDoubleEvenPlace(long int number)
 {
 int sum = 0;
